5549e4d36fddd62b4f8e306fad4311764da6843e
watsuyo <dmwtpdmwtp24@gmail.com>
2021-02-07 00:33:18 +0900

    エラー解消

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 
 // eslint-disable-next-line
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: { Component: any, pageProps: any }) {
   return <Component {...pageProps} />
 }
 
